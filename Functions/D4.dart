e4a4b83a88d7e8a2cbfacca916e9344a58ceb7d4
Samarpan KC <110466655+SamarpanKc@users.noreply.github.com>
2023-12-25 20:58:11 +0545

    Add files via upload

--- Functions/D4.dart
+++ Functions/D4.dart
@@ -1,30 +1,69 @@
+// void main() {
+//   print("Welcome!");
+
+//   // accesing class with variable
+//   var myC = Myclass();
+
+//   // calling function
+//   myC.printName("Samarpan KC");
+
+//   //
+//   //
+//   //
+//   //
+//   //
+//   myC.printName("BCA");
+//   //
+//   //
+
+//   myC.printName("SemI");
+//   //
+//   //
+//   myC.printName("Like this i call that same instruction many times");
+// }
+
+// class Myclass {
+//   void printName(String name) {
+//     // Function Deceleration
+//     print(name); // Function Defining
+//   }
+// }
+
+
 void main() {
-  print("Welcome!");
-
-  // accesing class with variable
-  var myC = Myclass();
-
-  // calling function
-  myC.printName("Samarpan KC");
-
-  //
-  //
-  //
-  //
-  //
-  myC.printName("BCA");
-  //
-  //
-
-  myC.printName("SemI");
-  //
-  //
-  myC.printName("Like this i call that same instruction many times");
+// 1
+  one();
+// 2
+  Second(43, 22);
+// 3
+  var dvid = Third();
+  print(dvid);
+// 4
+  var add = fourth(23, 65);
+  print(add);
 }
 
-class Myclass {
-  void printName(String name) {
-    // Function Deceleration
-    print(name); // Function Defining
-  }
+// TYPES
+// 1. No arguments No return type
+void one() {
+  int num1 = 20;
+  int num2 = 10;
+  print(num1 + num2);
+}
+
+// 2. With args No returntype
+void Second(num3, num4) {
+  print(num3 - num4);
+}
+
+// 3. No args With return type
+int Third() {
+  int numm = 22;
+  int numm1 = 4;
+  return numm % numm1;
+}
+
+// 4. With args with returntype
+int fourth(int numb, int numbe) {
+  return numb + numbe;
 }
--- Functions/Readme.md
+++ Functions/Readme.md
@@ -10,3 +10,46 @@
 3. Function Calling
 `var myC = Myclass();`
 `myC.printName("Samarpan KC");`
+
+## Purpose
+
+Functions in Dart are blocks of reusable code designed to perform a specific task. They help in organizing code, improving readability, and reducing redundancy.
+
+## Syntax
+
+The basic syntax of a function in Dart:
+- ```dart
+    returnType functionName(parameters) {
+        // Code to execute
+        return value; // Optional
+    }
+
+- `returnType:` The data type of the value the function returns. It can be void if the function doesn't return anything.
+- `functionName:` The name given to the function.
+- `parameters:` Inputs to the function, separated by commas if multiple
+
+## Example
+- ```dart
+    // Function without parameters and return type
+    void greet() {
+        print('Hello, Dart!');
+    }
+
+    // Function with parameters and return type
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    void main() {
+        greet(); // Output: Hello, Dart!
+        var sum = add(5, 3);
+        print('Sum: $sum'); // Output: Sum: 8
+    }
+
+## Key Points
+- Functions help in modularizing code for better organization and reusability.
+- They can accept parameters and return values.
+- Dart supports functions as first-class citizens, allowing functions to be assigned to variables or passed as arguments to other functions.
+- The `main()` function is the entry point of Dart programs.
+
+ Functions play a vital role in structuring Dart programs, facilitating code reuse and improving maintainability.
